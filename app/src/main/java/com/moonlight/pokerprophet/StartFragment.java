c44004fcde11df619332044a68e27edf74491cf6
flu5h <sabziro1995@mail.ru>
2019-10-10 00:06:52 +0300

    Start fragment navigation

--- app/src/main/java/com/moonlight/pokerprophet/StartFragment.java
+++ app/src/main/java/com/moonlight/pokerprophet/StartFragment.java
@@ -4,10 +4,12 @@ package com.moonlight.pokerprophet;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -15,6 +17,9 @@ import android.view.ViewGroup;
  */
 public class StartFragment extends Fragment {
 
+
+    private Button omaha_btn;
+    private Button holdem_btn;
 
     public StartFragment() {
         // Required empty public constructor
@@ -25,7 +30,16 @@ public class StartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+
+        omaha_btn = view.findViewById(R.id.omaha_btn);
+        omaha_btn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_startFragment_to_omahaFragment));
+        omaha_btn.setClickable(false);  //TODO
+
+        holdem_btn = view.findViewById(R.id.holdem_btn);
+        holdem_btn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_startFragment_to_holdemFragment));
+
+        return view;
     }
 
 }
