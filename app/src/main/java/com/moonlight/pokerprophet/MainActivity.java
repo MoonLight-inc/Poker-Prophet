205319935b257f9c05c4a7d93140b011e3b98afb
flu5h <sabziro1995@mail.ru>
2019-10-23 02:14:53 +0300

    init Ad

--- app/src/main/java/com/moonlight/pokerprophet/MainActivity.java
+++ app/src/main/java/com/moonlight/pokerprophet/MainActivity.java
@@ -3,12 +3,38 @@ package com.moonlight.pokerprophet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AdView adView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initUi();
+        initAd();
+
+    }
+
+    private void initUi() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+    }
+
+
+    private void initAd(){
+        MobileAds.initialize(this, getString(R.string.appId));
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 }
