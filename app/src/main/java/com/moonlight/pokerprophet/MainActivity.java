f711b2160174450ed70a4aded0b12515ac073361
flu5h <sabziro1995@mail.ru>
2019-11-02 02:39:25 +0300

    Revert "Drawer menu + toolbar = Road to Rules Activity"
    This reverts commit f8d2f5f3

--- app/src/main/java/com/moonlight/pokerprophet/MainActivity.java
+++ app/src/main/java/com/moonlight/pokerprophet/MainActivity.java
@@ -1,74 +1,30 @@
 package com.moonlight.pokerprophet;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AdView adView;
-    private Toolbar toolbar;
-    private ActionBar actionBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initUi();
-        initToolbar();
         initAd();
-        initNavigationMenu();
 
-    }
-
-    private void initToolbar() {
-        toolbar = findViewById(R.id.toolbar);
-        //toolbar.setNavigationIcon(R.drawable.ic_menu);
-        toolbar.setNavigationIcon(R.drawable.ic_menu);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Poker Prophet");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-    }
-
-    private void initNavigationMenu() {
-        NavigationView nav_view = findViewById(R.id.menu_view);
-        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_str, R.string.close_str) {
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-            }
-        };
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(final MenuItem item) {
-                Toast.makeText(getApplicationContext(), item.getTitle() + " Selected", Toast.LENGTH_SHORT).show();
-                actionBar.setTitle(item.getTitle());
-                drawer.closeDrawers();
-                return true;
-            }
-        });
-
-        // open drawer at start
-        drawer.openDrawer(GravityCompat.START);
     }
 
     private void initUi() {
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
