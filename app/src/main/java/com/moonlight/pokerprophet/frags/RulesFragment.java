46f44db6feb8492e24a818788a37fbb96ca63377
flu5h <sabziro1995@mail.ru>
2019-11-04 04:00:42 +0300

    Rules fragment impl

--- app/src/main/java/com/moonlight/pokerprophet/frags/RulesFragment.java
+++ app/src/main/java/com/moonlight/pokerprophet/frags/RulesFragment.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.moonlight.pokerprophet.R;
 
 
@@ -17,12 +22,53 @@ public class RulesFragment extends Fragment {
     }
 
 
+    View root;
+    LinearLayout body, stage0, stage1, stage2, stage3;
+    TextView txt;
 
+    private FloatingActionButton fab;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rules, container, false);
+        root = inflater.inflate(R.layout.fragment_rules, container, false);
+
+        fab = root.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(root).popBackStack();
+            }
+        });
+
+
+        body = root.findViewById(R.id.body);
+        body.setAlpha(0);
+        //System.out.println(body.getChildCount()+);
+
+        stage0 = (LinearLayout) body.getChildAt(1);
+        ((TextView) stage0.getChildAt(0)).setText(R.string.stage0);
+        ((ImageView) stage0.getChildAt(1)).setImageResource(R.drawable.question);
+        ((TextView) stage0.getChildAt(2)).setText(R.string.stage0text);
+
+        stage1 = (LinearLayout) body.getChildAt(2);
+        ((TextView) stage1.getChildAt(0)).setText(R.string.stage1);
+        ((ImageView) stage1.getChildAt(1)).setImageResource(R.drawable.question);
+        ((TextView) stage1.getChildAt(2)).setText(R.string.stage1text);
+
+        stage2 = (LinearLayout) body.getChildAt(3);
+        ((TextView) stage2.getChildAt(0)).setText(R.string.stage2);
+        ((ImageView) stage2.getChildAt(1)).setImageResource(R.drawable.question);
+        ((TextView) stage2.getChildAt(2)).setText(R.string.stage2text);
+
+        stage3 = (LinearLayout) body.getChildAt(4);
+        ((TextView) stage3.getChildAt(0)).setText(R.string.stage3);
+        ((ImageView) stage3.getChildAt(1)).setImageResource(R.drawable.question);
+        ((TextView) stage3.getChildAt(2)).setText(R.string.stage3text);
+        body.animate().alpha(1).setDuration(300).start();
+
+        return root;
     }
+
 
 }
